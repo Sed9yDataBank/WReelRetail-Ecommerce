5162947965d3b83a21ebe77e68c8b5b0d4af4a6c
Sed9yDataBank <benzidsedki@gmail.com>
2020-05-02 23:27:38 +0100

    Product Controller W/ CORS Configg

--- src/main/java/com/wreel/retail/RetailApplication.java
+++ src/main/java/com/wreel/retail/RetailApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class RetailApplication {
@@ -14,8 +17,17 @@ public class RetailApplication {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(RetailApplication.class, args);
 	}
-
 }
